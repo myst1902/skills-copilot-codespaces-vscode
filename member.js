6f9df45c33e3338559a8793efb6905e4f167e605
myst1902 <37635826+myst1902@users.noreply.github.com>
2024-05-26 12:28:47 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,16 @@
+function skillsMember() {
+    const skills = [
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'React',
+        'Node.js',
+        'MongoDB',
+        'Express',
+        'SQL',
+        'Git',
+        'Python'
+    ];
+
+    return skills;
+}
